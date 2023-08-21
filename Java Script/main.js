fbc8d96887c5cfb308a98ed3a34c1086272a2031
Loraynne231056 <231056@virtualwindow.co.za>
2023-08-21 15:03:06 +0200

    Not done
    I wanted to implement the API today, but I'm feeling very sick. It's a struggle to sit upright, so I'm just going to submit what I have.

--- Java Script/main.js
+++ Java Script/main.js
@@ -7,27 +7,56 @@ const arrTrips = [
         name: "Icelandic Tour",
         locations: "Destinations: Reykjavik, Isafjordur, Akureyri, Husavik, Hofn, and more.", 
         duration: 7 + " days",
-        price: "R" + 7000,
-        description: "Have you ever wanted to see the hidden beauties of Iceland? This trip will take you all around this majestic island. Let's eat hangikjöt together and mull other the elves being apparently displeased by some offensive road they've deemed 'unimportant' and are now huanting the locals for. Never fear though, the goverment claims that 'Not all elves are nasty' so our cruises are not in danger of being sunk by 'Spoon Licker'.",
-        image: "Vik Iceland.jpg"
+        price: 7000,
+        description: "Have you ever wanted to see the hidden beauties of Iceland? This trip will take you all around this majestic island. Let's eat hangikjöt together and mull other the elves being apparently displeased by some offensive road they've deemed 'unimportant' and are now haunting the locals for. Never fear though, the goverment claims that 'Not all elves are nasty' so our cruises are not in danger of being sunk by 'Spoon Licker'.",
+        image: "Vik Iceland.jpg",
+        destinations: "arctic",
+        time:"2023-05-12"
     },
     {
         name: "Greenland Route",
         locations: "Destinations: Nuuk, Tasiilaq, Sisimiut, Qarqortoq, and Ilulissat", 
         duration: 10 + " days",
-        price: "R" + 13000,
+        price: 13000,
         description: "If you didn't know where Greenland was, this trip is for you. Finally discover the wonders of Greenland (geography failed you) and the lovely lack of greenery it has to offer. Discover glorious ice bergs that are no match for our 100% indestructable cruis ships, and visit Tang & Riis in Iceland... Oh wait, geography has failed me too. Join us and we can fail at geography together while eating hangikjöt in the wrong country.",
-        image: "Tang & Riis.jpg"
+        image: "Tang & Riis.jpg",
+        destinations: "arctic",
+        time: "2023-08-29"
     },
     {
-        name: "Icelandic Tour",
+        name: "Animal Tour",
         locations: "Destinations: Ranging from the Baltic Sea to Iceland and Greenland.", 
         duration: 2 + " weeks",
-        price: "R" + 21000,
+        price: 21000,
         description: "This tour goes to four different countries in search of arctic animals. You will get to see whales, harp seals, narwalls (the dollar store's verion of unicorns), and many more majestic animals that survive the coldest climates on earth. If you're thinking that this card doesn't contain my usual snark, it's because I'm tired and can't come up with good jokes.",
-        image: "Penguin.jpg"
+        image: "Penguin.jpg",
+        destinations: "baltic",
+        time: "2023-03-09"
+    },
+    {
+        name: "Baltic Cruise",
+        locations: "Destinations: Denmark, Norway, Sweden, Finland, and Poland", 
+        duration: 5 + " days",
+        price: 9000,
+        description: "Discover the beautiful lands surrounding the Blatic sea. Everyone keeps forgetting about the actual Baltic States, but for the purposes of this cruise we will also ignore them. Sorry Latvia and your (literally) bloody old flag, but we will have to skip you.",
+        image: "Beach.jpg",
+        destinations: "baltic",
+        time: "2023-10-01"
+    },
+    {
+        name: "Forgotten Lands Cruise",
+        locations: "Destinations: Latvia, Lithuania, and Estonia", 
+        duration: 15 + " Days",
+        price: 17000,
+        description: "Welcome to the forgotten lands cruise. Discover places that even the ship captain does not know about. Emmerse yourself in that glorious brackish water, the (not creepy at all) hill of crosses, the lush forests, bloody flags, lynxes, and of course the bears spas. Great Velyke while your at it and tell her she's a piece of shit for not giving me easter eggs that one year. I remember you Velyke. ",
+        image: "Tallin.jpg",
+        destinations: "baltic",
+        time: "2023-03-28"
     }
 ];
+
+let appliedFilter = "";
+let appliedSort = "time required";
 
 // ----------------------------------------------------------
 // Document Loads
@@ -39,20 +68,24 @@ $(document).ready(function(){
 // Hide the text on the cards when page loads
     $("#description").hide();
 
-
-    
-    loadTrips();
+    filterSortTrips(arrTrips);
 });
 // ----------------------------------------------------------  
 // Load trips
 // ---------------------------------------------------------- 
- function loadTrips(){
+ function loadTrips(tripsToShow){
 
-    console.log(arrTrips);
+    console.log(tripsToShow);
 
-    for (let i = 0; i < arrTrips.length; i++) {
+    // Clear the Elements in the Container
 
-        const trip = arrTrips[i];
+    $("#tripContainer").empty();
+
+    // Loop through the shit
+
+    for (let i = 0; i < tripsToShow.length; i++) {
+
+        const trip = tripsToShow[i];
         
         console.log(trip);
 
@@ -60,7 +93,7 @@ $(document).ready(function(){
         $("#tripContainer").append($("#tripTemplate").html());
 
         // variable for current trip
-        let currentTrip = $("#tripContainer").children().eq(i+1);
+        let currentTrip = $("#tripContainer").children().eq(i);
 
         // Content for current trip array
         currentTrip.find("#title").text(trip.name);
@@ -75,9 +108,70 @@ $(document).ready(function(){
     }
  };
 
+// ----------------------------------------------------------  
+// When a Filters or Sorts is Clicked
+// ---------------------------------------------------------- 
+
+$("input[name='filterRadio']").click(function(){
+
+    appliedFilter = $(this).attr('value');
+
+    console.log(appliedFilter);
+
+    filterSortTrips();
+
+});
+
+$("input[name='sortRadio']").click(function(){
+
+    appliedSort = $(this).attr('value');
+
+    console.log(appliedSort);
+
+    filterSortTrips();
+
+});
+
+function filterSortTrips (){
+
+    let filterSortArrTrips = [];
+
+    // Filter the Trips
+
+    if(appliedFilter){
+        filterSortArrTrips = arrTrips.filter(trip => trip.destinations == appliedFilter);
+    } else {
+        filterSortArrTrips = arrTrips;
+    }
+
+    // Sort the Trips
+
+    if(appliedSort == "low to high"){
+
+        // Sort the Trips from Low to High Cost
+        filterSortArrTrips = filterSortArrTrips.sort((a,b) => {
+            return a.price - b.price;
+        });
+
+     } else if(appliedSort == "time required") {
+
+        // Sort Trips from Short to Long Trips
+        filterSortArrTrips = filterSortArrTrips.sort((a,b) => {
+            let da = new Date(a.time);
+            let db = new Date(b.time);
+
+            return db - da;
+     });
+    }
+
+    loadTrips(filterSortArrTrips);
+};
+
+
+
 
 // ----------------------------------------------------------  
-// When Car is Clicked
+// When Card is Clicked
 // ---------------------------------------------------------- 
 
 $("#tripContainer").on('click', '.card', function(){
@@ -87,7 +181,21 @@ $("#tripContainer").on('click', '.card', function(){
     $(this).find("#duration").toggle();
     $(this).find("#location").toggle();
     $(this).find("#description").toggle();
-    
 
+});
+
+// Weather Thing 
+
+$(document).ready(function(){
+
+$.azax({
+    type:"GET",
+    url:"https://api.openweathermap.org/data/2.5/weather?q=Riga&appid=2e69876c9344bbafe48bcf1b99faf2b1",
+    success:function(data){
+        console.log(data);
+    }
+}).done(function(){
+
+})
 
 });
