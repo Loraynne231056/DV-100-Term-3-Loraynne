423abc31cade50a817d6c26187eb23830ab79d68
Loraynne231056 <231056@virtualwindow.co.za>
2023-09-03 22:08:37 +0200

    Final Commit
    I still don't know what's up with that scroll bar... bars. There's more now. Yay. I blame the little asshole logo.

--- Java Script/main.js
+++ Java Script/main.js
@@ -5,52 +5,62 @@
 const arrTrips = [
     {
         name: "Icelandic Tour",
-        locations: "Destinations: Reykjavik, Isafjordur, Akureyri, Husavik, Hofn, and more.", 
-        duration: 7 + " days",
+        locations: "Destinations: Reykjavik, Iceland", 
+        duration: 10 + " Days",
         price: 7000,
-        description: "Have you ever wanted to see the hidden beauties of Iceland? This trip will take you all around this majestic island. Let's eat hangikjöt together and mull other the elves being apparently displeased by some offensive road they've deemed 'unimportant' and are now haunting the locals for. Never fear though, the goverment claims that 'Not all elves are nasty' so our cruises are not in danger of being sunk by 'Spoon Licker'.",
+        description: "Have you ever wanted to go to insufferably cold place like Island? Let's eat hangikjöt together and mull other the elves being apparently displeased by some offensive road they've deemed 'unimportant' and are now haunting the locals for. Never fear though, the goverment claims that 'Not all elves are nasty' so our cruises are not in danger of being sunk by 'Spoon Licker'.",
         image: "Vik Iceland.jpg",
-        destinations: "arctic",
+        destinations: "single",
         time:"2023-05-12"
     },
     {
         name: "Greenland Route",
-        locations: "Destinations: Nuuk, Tasiilaq, Sisimiut, Qarqortoq, and Ilulissat", 
-        duration: 10 + " days",
+        locations: "Destinations: Nuuk, Greenland", 
+        duration: 7 + " Days",
         price: 13000,
-        description: "If you didn't know where Greenland was, this trip is for you. Finally discover the wonders of Greenland (geography failed you) and the lovely lack of greenery it has to offer. Discover glorious ice bergs that are no match for our 100% indestructable cruis ships, and visit Tang & Riis in Iceland... Oh wait, geography has failed me too. Join us and we can fail at geography together while eating hangikjöt in the wrong country.",
+        description: "If you didn't know where Greenland was, this trip is for you. Finally discover Greenland and the lovely lack of greenery it has to offer. Discover glorious ice bergs that are no match for our 100% indestructable cruis ships, and visit Tang & Riis in Iceland... Oh wait, geography has failed me too. Join us and geography can fail us together while we eat hangikjöt on the wrong island.",
         image: "Tang & Riis.jpg",
-        destinations: "arctic",
+        destinations: "single",
         time: "2023-08-29"
     },
     {
         name: "Animal Tour",
-        locations: "Destinations: Ranging from the Baltic Sea to Iceland and Greenland.", 
-        duration: 2 + " weeks",
+        locations: "Destinations: Ranging from the Baltic Sea to Iceland and back", 
+        duration: 18 + " Days",
         price: 21000,
-        description: "This tour goes to four different countries in search of arctic animals. You will get to see whales, harp seals, narwalls (the dollar store's verion of unicorns), and many more majestic animals that survive the coldest climates on earth. If you're thinking that this card doesn't contain my usual snark, it's because I'm tired and my life is sad (today). If you don't want to be sad, you better pay up that R21000 and join the cruise. Please pay me.",
+        description: "This tour goes to four different countries in search of arctic animals. You will get to see whales, harp seals, narwalls (the dollar store's verion of unicorns), and many more majestic animals that survive the coldest climates on earth. If you're thinking this card doesn't contain snark, it's because I'm tired and my life is sad. If you don't want to be sad, you better pay up and join the cruise.",
         image: "Penguin.jpg",
-        destinations: "baltic",
+        destinations: "round",
         time: "2023-03-09"
     },
     {
         name: "Baltic Cruise",
         locations: "Destinations: Denmark, Norway, Sweden, Finland, and Poland", 
-        duration: 5 + " days",
+        duration: 5 + " Days",
         price: 9000,
         description: "Discover the beautiful lands surrounding the Blatic sea. Everyone keeps forgetting about the actual Baltic States, but for the purposes of this cruise we will also ignore them. Sorry Latvia and your (literally) bloody old flag, but we will have to skip you.",
         image: "Beach.jpg",
-        destinations: "baltic",
+        destinations: "multi",
         time: "2023-10-01"
     },
     {
         name: "Forgotten Lands Cruise",
         locations: "Destinations: Latvia, Lithuania, and Estonia", 
-        duration: 15 + " Days",
+        duration: 14 + " Days",
         price: 17000,
-        description: "Welcome to the forgotten lands cruise. Discover places that even the ship captain does not know about. Emmerse yourself in that glorious brackish water, the (not creepy at all) hill of crosses, the lush forests, bloody flags, lynxes, and of course the bears spas. Great Velyke while your at it and tell her she's a piece of shit for not giving me easter eggs that one year. I remember you Velyke. ",
+        description: "Welcome to the forgotten lands cruise. Discover places that even the ship captain does know exists. Emmerse yourself in that glorious brackish water, the (not creepy at all) hill of crosses, the lush forests, bloody flags, lynxes, and of course the bears spas. Great Velyke while your at it and tell her she's a piece of shit for not giving me easter eggs that one year. I remember you Velyke. ",
         image: "Tallin.jpg",
-        destinations: "baltic",
+        destinations: "multi",
+        time: "2023-03-28"
+    },
+    {
+        name: "Iceberg Tour",
+        locations: "Destinations: To the icebergs and back.", 
+        duration: 15 + " Days",
+        price: 16000,
+        description: "Let's go see some majestic Icebergs and come back! Emphasis on coming back. Did you know that going to view icebergs is objectively a better life decision than having to deal with that bloody guy Dave. Oh yes... Dave the anthropomorphic version of dev. His mother must be proud.",
+        image: "Iceberg.jpg",
+        destinations: "round",
         time: "2023-03-28"
     }
 ];
@@ -83,8 +93,17 @@ $(document).ready(function(){
 
 // Hide the text on the cards when page loads
     $("#description").hide();
+    $("#cardButton").hide();
 
     filterSortTrips(arrTrips);
+
+// Hide the text on the carousel
+setTimeout(function(){
+    $("#vacation").fadeOut(400, function(){
+        $("#vacation").text('Welcome to your Cruise Bob.').fadeIn(400);
+    });
+},5000);
+
 });
 // ----------------------------------------------------------  
 // Load trips
@@ -121,6 +140,15 @@ $(document).ready(function(){
 
         // Hide discription text from current trip 
         currentTrip.find("#description").hide();
+        currentTrip.find("#cardButton").hide();
+
+        $(".evilLogo").hover(function() { 
+            $(this).attr("src", "../Assets/" + "LogoWhite.png"); 
+          }, 
+          function() { 
+            $(this).attr("src", "../Assets/" + "LogoBlack.png"); 
+          } 
+        ); 
     }
  };
 
@@ -197,6 +225,8 @@ $("#tripContainer").on('click', '.card', function(){
     $(this).find("#duration").toggle();
     $(this).find("#location").toggle();
     $(this).find("#description").toggle();
+    $(this).find("#cardButton").toggle();
+   
 
 });
 
@@ -233,7 +263,7 @@ function loadWeatherThing(){
         $(".weatherFucker").append($("#weatherTemp").html());
 
         // variable for current weather
-        let currentWeather = $(".weatherFucker").children().eq(i+1);
+        let currentWeather = $(".weatherFucker").children().eq(i+2);
 
         // Content for current trip array
         currentWeather.find(".weatherPic").attr('src', weather.weatherImage);
@@ -242,3 +272,25 @@ function loadWeatherThing(){
         
     }
  };
+
+
+// Hide cart items
+
+$(".cancel-one").click(function(){
+    $(".minus-first").remove();
+  })
+  $(".cancel-two").click(function(){
+    $(".minus-second").remove();
+  })
+  $(".cancel-three").click(function(){
+    $(".minus-third").remove();
+  })
+  $(".cancel-four").click(function(){
+    $(".minus-fourth").remove();
+  })
+  $(".cancel-all").click(function(){
+    $(".minus-first, .minus-second, .minus-third, .minus-fourth").remove();
+  })
+
+  // Total Cost
+
--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # DV-100-Term-3-Loraynne
+
+# About the Project
+
+### The project is a website for a cruise liner company. The company is called Nautica and it specialises in arctic and baltic cruises. I'm telling you, icebergs are weak compared to our indestructable cruise ships. Geez Bob. Your safety is 100% garanteed.
+
+## **Home Page**
+The hero page has a carousel slider, a gallery, an FAQ accordion, and weather cards. 
+
+### Hero Slider:
+
+![Hero](Assets/Hero.png)
+
+### Gallery:
+
+![Hero](Assets/Gallery.png)
+
+### FAQ Accordion:
+![Hero](Assets/faq.png)
+
+### Weather API:
+![Hero](Assets/weather.png)
+
+## **Browse Page**
+The browse page contains trip cards with information on them. When you click the card a buy button and extra information appears. There is also a few filters and sorts the user can use to dfind the trip they're looking for.
+### Trip Cards:
+![Hero](Assets/cards.png)
+
+### On Click Extra Information:
+![Hero](Assets/infocards.png)
+
+## **Checkout Page**
+The checkout page has a table with your orders listed on it. The table contains information such as the name, quantity, duration, trip code, and duration of the trip. There is also a remove button and remove all button. There's a total price and a proceed to payment button that leads to a "Thank you" modal 
+### Order Table:
+![Hero](Assets/table.png)
+
+### Proceed Column:
+![Hero](Assets/proceed.png)
+
+# Getting Started
+
+### You would need some sort of electronic device and wifi or data. If you have that, you need to get GitHub. You can also get Visual Studio Code if you want to use the same software I am.
+
+# Built With
+
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+- Open Weatheer API
+- JQuery
+- Insomnia
+- Pexel
+- Unsplash
+# Contributing
+
+### If you require any assistance, please do not hesitate to contact me on 231056@virtualwindow.co.za for more information.
+
+# Authors
+
+- Loraynne van Tonder (Me)
+- Celeste van Tonder (My Sister)<br>
+  Helping me fix broken code & getting my shitty button to work.
+
+# Licence
+
+### MIT License
+
+# Acknowledgement
+
+- Tarryn-Leigh-Tong (My Friend)<br>
+  Thank you for the moral support and helping me with that guy, Dave!
+- Karlo Kyuper (Teacher's Asisstant)
+- Paul Roots (Lecturer)
+- Dr. Google
+- Naruto Guy on the internet
+
+# Protoype Video
+My Loom account expired and now I have to pay to download stuff, so this is perfect. Just click the link!
+https://www.loom.com/share/5847d39477644ac6a5eff73f70e7b0ca?t=285&sid=fbdd44d2-b073-4ea2-a266-1ab9dded97a8
+
+# Plagiarism Document
+https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:578e7838-4660-3288-8dff-da6df4e4d1bb
+
+# Some References
+Hey there! All my images are from Unsplash and Pexels. My logo is from Adobe Logo Maker. (That was a mistake. The logo was an absolute little asshole.) Finally, my icons are from the Bootstrap icons.
+
+
+Okay cool people! Enjoy marking! Bye ^^
